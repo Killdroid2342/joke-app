df1850dcb0063ee0b6ce90e4bb53590750ec6e9a
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-10-29 21:50:06 +0100

    "finished title"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Joke from './components/Joke/Joke';
 import Selector from './components/Selector/Selector';
-import { useState } from 'react';
+import React, { useState } from 'react';
 function App() {
   const [genre, setGenre] = useState('Dark');
 
   return (
-    <div className='main-app'>
-      <h1>Wanna Hear a joke?</h1>
+    <React.Fragment>
+      <h1 className='text-center text-3xl font-bold mt-10'>
+        Wanna Hear a joke?
+      </h1>
       <Selector genre={genre} setGenre={setGenre} />
       <div className='under-selector'>
         <Joke genre={genre} />
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
