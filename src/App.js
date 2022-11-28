991e00665ca7772f93f1fa7f6295128f7d02e760
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-28 20:38:25 +0000

    removed all styling from appJS

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ function App() {
 
   return (
     <React.Fragment>
-      <h1 className='text-center text-3xl font-bold mt-10'>
-        Wanna Hear a joke?
-      </h1>
+      <h1>Wanna Hear a joke?</h1>
       <Selector genre={genre} setGenre={setGenre} />
-      <div className='border border-red-800 flex flex-row justify-center'>
+      <div>
         <Joke genre={genre} />
       </div>
     </React.Fragment>
