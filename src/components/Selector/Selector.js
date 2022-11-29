5cb1901a75f676a0e4afb7e7581bb0c98ee8abef
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-29 02:44:55 +0000

    spacing between the selectors

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -1,50 +1,57 @@
 import React from 'react';
 const Selector = ({ genre, setGenre }) => {
   return (
-    <form>
-      <div>
-        <label>Dark: </label>
-        <input
-          type='radio'
-          name='gender'
-          value='Dark'
-          onChange={() => setGenre('Dark')}
-          checked={genre === 'Dark'}
-        />
-        <label>Misc:</label>
-        <input
-          type='radio'
-          name='gender'
-          value='Misc'
-          onChange={() => setGenre('Misc')}
-          checked={genre === 'Misc'}
-        />
-        <label>Programming:</label>
-        <input
-          type='radio'
-          name='gender'
-          value='Programming'
-          onChange={() => setGenre('Programming')}
-          checked={genre === 'Programming'}
-        />
-        <label>Pun:</label>
-        <input
-          type='radio'
-          name='gender'
-          value='Pun'
-          onChange={() => setGenre('Pun')}
-          checked={genre === 'Pun'}
-        />
-        <label>Spooky:</label>
-        <input
-          type='radio'
-          name='gender'
-          value='Spooky'
-          onChange={() => setGenre('Spooky')}
-          checked={genre === 'Spooky'}
-        />
-      </div>
-    </form>
+    <div className='text-center'>
+      <form>
+        <div className='p-5'>
+          <label>Dark: </label>
+          <input
+            type='radio'
+            name='gender'
+            value='Dark'
+            onChange={() => setGenre('Dark')}
+            checked={genre === 'Dark'}
+            className='m-2'
+          />
+          <label>Misc:</label>
+          <input
+            type='radio'
+            name='gender'
+            value='Misc'
+            onChange={() => setGenre('Misc')}
+            checked={genre === 'Misc'}
+            className='m-2'
+          />
+          <label>Programming:</label>
+          <input
+            type='radio'
+            name='gender'
+            value='Programming'
+            onChange={() => setGenre('Programming')}
+            checked={genre === 'Programming'}
+            className='m-2'
+          />
+          <label>Pun:</label>
+          <input
+            type='radio'
+            name='gender'
+            value='Pun'
+            onChange={() => setGenre('Pun')}
+            checked={genre === 'Pun'}
+            className='m-2'
+          />
+          <label>Spooky:</label>
+          <input
+            type='radio'
+            name='gender'
+            value='Spooky'
+            onChange={() => setGenre('Spooky')}
+            checked={genre === 'Spooky'}
+            className='m-2'
+          />
+        </div>
+      </form>
+    </div>
   );
 };
 
