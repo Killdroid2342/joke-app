8f983eefe95fd0affa40051227cc742e4481eb3d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-10 10:29:07 +0000

    created props for setup

--- src/components/Joke/JokeSetup/JokeSetup.js
+++ src/components/Joke/JokeSetup/JokeSetup.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const JokeSetup = () => {
-  return <div></div>;
+const JokeSetup = ({ joke }) => {
+  return (
+    <div>
+      <ul className='border-2 border-white w-56 rounded-xl mt-5'>
+        {joke && (
+          <div className='p-5'>
+            {joke.type === 'single' ? (
+              <li>{joke.joke}</li>
+            ) : (
+              <div>
+                <li>{joke.setup}</li>
+                <li>{joke.delivery}</li>
+              </div>
+            )}
+          </div>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default JokeSetup;
