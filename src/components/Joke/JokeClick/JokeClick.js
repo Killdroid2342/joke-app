e91763ebdaf53292148a381a2ce458f79e79258b
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-10 10:29:00 +0000

    created props for click

--- src/components/Joke/JokeClick/JokeClick.js
+++ src/components/Joke/JokeClick/JokeClick.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const JokeClick = () => {
-  return <div></div>;
+const JokeClick = ({ setCount, count, call, saveJoke }) => {
+  return (
+    <div className='flex flex-col'>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          call();
+        }}
+        className='border-2 border-white p-3 mt-5 rounded-xl font-bold'
+      >
+        Click For New Joke
+      </button>
+      <button
+        onClick={saveJoke}
+        className='border-2 border-white p-3 mt-5 rounded-xl font-bold'
+      >
+        Save current Joke
+      </button>
+    </div>
+  );
 };
 
 export default JokeClick;
