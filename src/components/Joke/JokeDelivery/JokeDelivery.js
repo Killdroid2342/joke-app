71aa9b76b911c1214f75bd90f633dfcc727e92ab
Haroon Khan <HaroonKhan120704@gmail.com>
2023-01-11 08:08:36 +0000

    put jsx in other file

--- src/components/Joke/JokeDelivery/JokeDelivery.js
+++ src/components/Joke/JokeDelivery/JokeDelivery.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const JokeDelivery = () => {
-  return <div></div>;
+const JokeDelivery = ({ savedJokes }) => {
+  return (
+    <div>
+      <p className='text-3xl mt-5 mb-5 font-bold'>Saved Jokes</p>
+      {savedJokes?.map((savedJoke, idx) => {
+        return (
+          <div key={idx}>
+            {savedJoke.type === 'single' ? (
+              <li>{savedJoke.joke}</li>
+            ) : (
+              <div className='border-2 border-white p-5 rounded-lg mb-5'>
+                <li className='list-none'>{savedJoke.setup}</li>
+                <li className='list-none'>{savedJoke.delivery}</li>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default JokeDelivery;
